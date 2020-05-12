ea50d9472305130fd057d4d148f61ae281b17022
DEV-black-ops <64575690+DEV-black-ops@users.noreply.github.com>
2020-05-12 15:07:01 +0530

    Add files via upload

---
+++ cv2 .py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+img = cv2.imread('C:/Users/Dr.Stark/Desktop/cv 2.jpg',1)
+cv2.imshow('image',img)
+
+
+# In[ ]:
+
+
+k = cv2.waitKey(0)
+if k == 26:
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    value = 10
+    cv2.add(hsv[:,:,2], value, hsv[:,:,2])
+    image = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    cv2.imwrite('out.png', image)
+    
+elif k == 27:
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    value = 10
+    cv2.sub(hsv[:,:,2], value, hsv[:,:,2])
+    image = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    cv2.imwrite('out.png', image)
+   
+
+
+    
+
+
+# # solution 3
+
+# In[ ]:
+
+
+import cv2
+
+
+img_grey = cv2.imread('C:/Users/Dr.Stark/Desktop/cv 2.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imwrite('C:/Users/Dr.Stark/Desktop/cv 3.jpg',img_grey)
+
+thresh = 128
+
+
+img_binary = cv2.threshold(img_grey, thresh, 255, cv2.THRESH_BINARY)[1]
+
+
+cv2.imwrite('C:/Users/Dr.Stark/Desktop/cv 4.jpg',img_binary)
+
+
+# In[ ]:
+
+
+img[:,:,2] = np.zeros([img.shape[0], img.shape[1]])
+
+
+# In[ ]:
+
+
+img[:,:,1] = np.zeros([img.shape[0], img.shape[1]])
+
+
+# In[ ]:
+
+
+cv2.imwrite('C:/Users/Dr.Stark/Desktop/cv 5.jpg',img_)
+
+
+# In[ ]:
+
+
+
+
+
+# # solution 4
+
+# In[ ]:
+
+
+import cv2 
+har = cv2.CascadeClassifier('D:cv2/opencv-master/opencv-master/data/haarcascades/haarcascade_frontalface_default.xml')
+img = cv2.imread('Desktop/many face image.jpg')
+
+
+# In[ ]:
+
+
+faces = har.detectMultiScale(img, 1.1, 4)
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    if cv2.waitKey(0)==25:
+            break
+print(len(faces))
+
+
+# In[ ]:
+
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
